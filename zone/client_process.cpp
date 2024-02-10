9343ee3154adee2838669ee7a4cfb2a9c8e9718f
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-10 01:24:05 -0600

    fuck

--- zone/client_process.cpp
+++ zone/client_process.cpp
@@ -1725,13 +1725,12 @@ void Client::OPGMTrainSkill(const EQApplicationPacket *app)
 		uint16 skilllevel = GetRawSkill(skill);
 
 		if (skilllevel == 0) {
-			//this is a new skill..				
-				uint16 t_level = SkillTrainLevel(skill, GetClass());
+			//this is a new skill..
+			uint16 t_level = SkillTrainLevel(skill, GetClass());
 
-				if (t_level == 0) {
-					LogSkills("Tried to train a new skill [{}] which is invalid for this race/class.", skill);
-					return;
-				}
+			if (t_level == 0) {
+				LogSkills("Tried to train a new skill [{}] which is invalid for this race/class.", skill);
+				return;
 			}
 
 			SetSkill(skill, t_level);
