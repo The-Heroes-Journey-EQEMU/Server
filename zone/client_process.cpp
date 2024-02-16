8c0bde682064ba41cce3f553503c11e94a8a8728
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-15 19:58:09 -0600

    f

--- zone/client_process.cpp
+++ zone/client_process.cpp
@@ -444,7 +444,7 @@ bool Client::Process() {
 			}
 		}
 
-		if (GetClassesBitmask() & (GetPlayerClassBit(Class::Berserker) | GetPlayerClassBit(Class::Warrior))) {
+		if (GetClassesBits() & (GetPlayerClassBit(Class::Berserker) | GetPlayerClassBit(Class::Warrior))) {
 			if (!dead && !IsBerserk() && GetHPRatio() < RuleI(Combat, BerserkerFrenzyStart)) {
 				entity_list.MessageCloseString(this, false, 200, 0, BERSERK_START, GetName());
 				berserk = true;
