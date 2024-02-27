988973baa1c175864d59aa18fc79fe45968dd55e
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-26 19:04:31 -0600

    f

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -142,10 +142,6 @@ int Mob::GetSpellImpliedTargetID(uint16 spell_id, uint16 target_id) {
 	auto spell = spells[spell_id];
 	auto caster = CastToClient();
 
-	if ((item_slot != -1 && cast_time == 0) || aa_id) {
-		send_spellbar_enable = false;
-	}
-
 	// Early Checks
 	if (IsClient() && RuleB(Spells, UseSpellImpliedTargeting)) {
 		if (spell.target_type == ST_Pet || spell.target_type == ST_SummonedPet) {
