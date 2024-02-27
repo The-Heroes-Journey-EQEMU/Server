f1a8e02e4e257671f8768e8316333aea1c12e223
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-02-26 19:37:30 -0600

    wtf

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -147,9 +147,9 @@ int Mob::GetSpellImpliedTargetID(uint16 spell_id, uint16 target_id) {
 	if (IsClient() && RuleB(Spells, UseSpellImpliedTargeting)) {
 		if (spell.target_type == ST_Pet || spell.target_type == ST_SummonedPet) {
 			LogDebug("This is a pet spell.... wtf");
-			if (CastToClient()->GetPetID()) {
-				LogDebug("We have a pet");
-				return CastToClient()->GetPetID();
+			if (GetPet()) {
+				LogDebug("We have a pet.. returning {}", GetPetID());
+				return GetPetID();
 			} else {
 				LogDebug("We don't have a pet???");
 				return -1;
