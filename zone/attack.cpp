32fd7c58d0575b376e465abb6e5658a062904627
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-09 01:23:36 -0600

    test

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -4509,9 +4509,8 @@ void Mob::CommonDamage(Mob* attacker, int64 &damage, const uint16 spell_id, cons
 							FilterPetSpells,
 							OTHER_HIT_NONMELEE, /* %1 hit %2 for %3 points of non-melee damage. */
 							0,
-							attacker->GetCleanName(), /* Message1 */
-							GetCleanName(), /* Message2 */
-							ConvertArray(damage, val1) /* Message3 */
+							GetCleanName(), /* Message1 */
+							ConvertArray(damage, val1) /* Message2 */
 						);
 					}
 				} else {
