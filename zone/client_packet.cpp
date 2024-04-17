69e03dd3c42faa564cf9f6007ca3e439f61cc961
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-17 00:21:12 -0500

    oh maybe this?

--- zone/client_packet.cpp
+++ zone/client_packet.cpp
@@ -10910,23 +10910,25 @@ void Client::Handle_OP_MoveMultipleItems(const EQApplicationPacket *app)
 		// This is checked by the client, but can't hurt to check here too.
 		if (m_inv.GetItem(from_bag)->IsClassBag() && m_inv.GetItem(to_bag)->IsClassBag()) {
 			for (int i = 0; i < multi_move->count; i++) {
-				MoveItem_Struct* mi = new MoveItem_Struct();
-				mi->from_slot = m_inv.CalcSlotId(multi_move->moves[i].from_slot.Slot, multi_move->moves[i].from_slot.SubIndex);
-				mi->to_slot   = m_inv.CalcSlotId(multi_move->moves[i].to_slot.Slot, multi_move->moves[i].to_slot.SubIndex);
-				mi->number_in_stack = 0; // This is always 0 in MoveItem_Struct unless we are combining stacks, which this never tries to do.
+				if (multi_move->moves[i].from_slot.Slot != EQ::invslot::slotCursor) {
+					MoveItem_Struct* mi = new MoveItem_Struct();
+					mi->from_slot = m_inv.CalcSlotId(multi_move->moves[i].from_slot.Slot, multi_move->moves[i].from_slot.SubIndex);
+					mi->to_slot   = m_inv.CalcSlotId(multi_move->moves[i].to_slot.Slot, multi_move->moves[i].to_slot.SubIndex);
+					mi->number_in_stack = 0; // This is always 0 in MoveItem_Struct unless we are combining stacks, which this never tries to do.
 
-				LogInventory("Swapping slot [{}] to slot [{}]",mi->from_slot,mi->to_slot);
+					LogInventory("Swapping slot [{}] to slot [{}]",mi->from_slot,mi->to_slot);
 
-				if (!SwapItem(mi) && IsValidSlot(mi->from_slot) && IsValidSlot(mi->to_slot)) {
-					SwapItemResync(mi);
+					if (!SwapItem(mi) && IsValidSlot(mi->from_slot) && IsValidSlot(mi->to_slot)) {
+						SwapItemResync(mi);
 
-					bool error = false;
-					InterrogateInventory(this, false, true, false, error, false);
-					if (error)
-						InterrogateInventory(this, true, false, true, error);
+						bool error = false;
+						InterrogateInventory(this, false, true, false, error, false);
+						if (error)
+							InterrogateInventory(this, true, false, true, error);
+					}
+
+					safe_delete(mi);
 				}
-
-				safe_delete(mi);
 			}
 			return;
 		} else {
