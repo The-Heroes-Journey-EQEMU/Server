f0d20e5b1cec06c427bf35d896bb898870c38e73
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-29 12:37:15 -0500

    experience message

--- zone/exp.cpp
+++ zone/exp.cpp
@@ -509,15 +509,18 @@ void Client::AddEXP(uint64 in_add_exp, uint8 conlevel, bool resexp) {
 		if (upgrade_item) {
 			int cur_item_exp   = in_add_exp + Strings::ToInt(upgrade_item->GetCustomData("Item_Experience"));
 			int tar_item_exp   = upgrade_item->GetMutableItem()->CalculateGearScore() * 10000;
-
-
 			double epercentage = cur_item_exp / tar_item_exp;
 			
 			upgrade_item->SetCustomData("Item_Experience", cur_item_exp);
 			database.UpdateInventorySlot(CharacterID(), upgrade_item, EQ::invslot::slotPowerSource);
 
+			LogDebug("cur_item_exp [{}], tar_item_exp [{}], epercentage [{}]", cur_item_exp, tar_item_exp, epercentage);
 
-			LogDebug("cur_item_exp [{}], tar_item_exp [{}], epercentage [{}]", cur_item_exp, tar_item_exp, epercentage);			
+
+			EQ::SayLinkEngine linker;
+			linker.SetLinkType(EQ::saylink::SayLinkItemInst);
+			linker.SetItemInst(upgrade_item);
+			Message(Chat::Experience, "Your [%s] has gained experience!", linker.GenerateLink().c_str());			
 			return;
 		}
 	}	
