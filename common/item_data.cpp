ba4175de364abc7786cccef46ec294c9b112e214
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-29 15:49:29 -0500

    test

--- common/item_data.cpp
+++ common/item_data.cpp
@@ -262,12 +262,12 @@ const uint64 EQ::ItemData::CalculateGearScore() const {
 	int gear_score = 5;
 	
 	// Basic Stats
-	//gear_score += 5   * AStr + ASta + ADex + AAgi + AInt + AWis + ACha;
-	//gear_score += 2   * MR + FR + CR + DR + PR;
-	//gear_score += 2   * HP + Mana + Endur;
-	//gear_score += 5   * Damage;
-	//gear_score += 10  * Attack + SpellDmg + HealAmt;
-	//gear_score += 10  * (AC + ExtraDmgAmt + BaneDmgRaceAmt + ElemDmgAmt);
+	gear_score += 5   * AStr + ASta + ADex + AAgi + AInt + AWis + ACha;
+	gear_score += 2   * MR + FR + CR + DR + PR;
+	gear_score += 2   * HP + Mana + Endur;
+	gear_score += 5   * Damage;
+	gear_score += 10  * Attack + SpellDmg + HealAmt;
+	gear_score += 10  * AC;
 	//gear_score += 100 * Haste;
 
 	// Heroic Stats
