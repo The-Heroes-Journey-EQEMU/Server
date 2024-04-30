4975cb41bbf620371f10cfd2c58fcb5e63a9202d
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-29 21:10:50 -0500

    don't add items to mob loot tables on trade

--- zone/trading.cpp
+++ zone/trading.cpp
@@ -849,15 +849,17 @@ void Client::FinishTrade(Mob* tradingWith, bool finalizer, void* event_entry, st
 							}
 						}
 
-						auto new_loot_drop_entry = LootdropEntriesRepository::NewNpcEntity();
-						new_loot_drop_entry.equip_item = 1;
-						new_loot_drop_entry.item_charges = static_cast<int8>(inst->GetCharges());
+						if (tradingWith->IsPet()) {
+							auto new_loot_drop_entry = LootdropEntriesRepository::NewNpcEntity();
+							new_loot_drop_entry.equip_item = 1;
+							new_loot_drop_entry.item_charges = static_cast<int8>(inst->GetCharges());
 
-						tradingWith->CastToNPC()->AddLootDropFixed(
-							item,
-							new_loot_drop_entry,
-							true
-						);
+							tradingWith->CastToNPC()->AddLootDropFixed(
+								item,
+								new_loot_drop_entry,
+								true
+							);
+						}
 					}
 				
 					// Return quest items being traded to non-quest NPC when the rule is true
