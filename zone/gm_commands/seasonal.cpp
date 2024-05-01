75881ece89f82eee6d391dd62f306e2734bb1fae
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-05-01 01:09:53 -0500

    typos

--- zone/gm_commands/seasonal.cpp
+++ zone/gm_commands/seasonal.cpp
@@ -19,11 +19,11 @@ void command_disable_seasonal(Client *c, const Seperator *sep)
 void command_seasoninfo(Client *c, const Seperator *sep)
 {
 	DataBucketKey k;
-	k.character_id 	= c->CharacterID;
+	k.character_id 	= c->CharacterID();
 	k.key 		 	= "Season-LoginCount";
 
 	int arguments   = sep->argnum;
-	int login_count = DataBucket::GetData(k);
+	int login_count = Strings::ToInt(DataBucket::GetData(k).value);
 
 	c->Message(Chat::White, "Welcome to Retribution's first seasonal event! This is a special, time-limited event with character and time-locked progression, unique rewards, and unique challenges! " +
 							"You may not access certain features as a seasonal character, nor meaningfully interact with non-seasonal characters. See the Discord server for more information! " +
