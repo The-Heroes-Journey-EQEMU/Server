b0eb41d9bd08397d57349cd52c5644ecbf15fc7a
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-05-01 01:50:59 -0500

    test disco order

--- zone/inventory.cpp
+++ zone/inventory.cpp
@@ -721,12 +721,12 @@ bool Client::SummonItem(uint32 item_id, int16 charges, uint32 aug1, uint32 aug2,
 	}
 	
 	if (!GetGM()) {
-		if (RuleB(Character, EnableDiscoveredItems)) {
-			DiscoverItem(inst->GetItem()->ID);
-		}
-
 		if (artifact_disco) {
 			DiscoverArtifact(inst);
+		}
+
+		if (RuleB(Character, EnableDiscoveredItems)) {
+			DiscoverItem(inst->GetItem()->ID);
 		}
 	}
 
